e20cabacceafbcfd8edca74a0a41a9523d83fd33
helmi.bouhlel1 <helmi.bouhlel@enicar.ucar.tn>
2026-06-10 14:46:58 +0100

    Remove Alert-Telemetry-Ota V0 -Nv-Firmware from repository

--- Alert-Telemetry-Ota V0 -Nv-Firmware/include/secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-// ============================================================================
-//  WI-FI
-// ============================================================================
-#define WIFI_SSID            "NA Stagiaires"
-#define WIFI_PASSWORD        "$tage@N*2023*A"
-#endif
